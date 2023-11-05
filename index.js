1e007e47255a5bcf0bd96fd629f9c09415479e5a
unknown <shaulakelo@gmail.com>
2023-11-06 01:46:53 +0300

    well this isn't going as planned

--- index.js
+++ index.js
@@ -17,17 +17,60 @@ const platforms = [
     }
 ]
 
-const services = document.querySelectorAll(".grid-item")
+const services = document.querySelectorAll(".grid-title")
 const select_service = document.querySelectorAll(".service-selected")
 const default_content = document.querySelector(".default-content")
+const countries = document.querySelectorAll(".country-title")
+const select_country = document.querySelectorAll(".selected-country")
+const country_btn = document.querySelectorAll(".country-button")
+const info_temp = document.querySelector(".info-temp")
+const info_temp_1 = document.querySelector(".info-temp-1")
+const temp_triad = document.querySelector(".temp-triad");
+const how_select_service = document.querySelector(".how-select-service")
+
+let is_selected = false;
+let is_selected_1 = false;
+
 services.forEach((service) =>{
     service.addEventListener("click", ()=>{
         default_content.style.display = "none";
         select_service.forEach(selected =>{
-           selected.innerText = service.textContent; 
+           selected.innerText = "for " + service.textContent; 
         })
+        is_selected = true;
+        
+        how_select_service.style.display = "block"
     })
 })
+
+country_btn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const country_item = btn.parentElement.parentElement;
+        const country_title = country_item.querySelector(".country-title")
+
+        default_content.style.display = "none";
+        info_temp.style.display = "none";
+        info_temp_1.style.display = "block"
+
+        select_country.forEach(selected => {
+            selected.innerText = "in " + country_title.textContent;
+        })
+
+        btn.innerHTML = '<div>Selected</div>';
+
+        temp_triad.style.display = "none"
+        how_select_service.style.display = "none"
+
+        is_selected_1 = true;
+
+        if(is_selected && is_selected_1){
+            how_select_service.style.display = "block"
+        }
+
+    })
+})
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const search_bar = document.getElementById("search_id")
